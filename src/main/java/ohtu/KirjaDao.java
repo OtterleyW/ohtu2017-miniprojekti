427ca45914cabc47223fe235dfad9e6dda767e8a
Peppi Mattsson <bebenoctescere@gmail.com>
2017-11-17 01:40:39 +0200

    muutin Kirja-taulun taas alkeelliseksi (id, kirjoittaja, otsikko), sitä voi sitten muokata

--- src/main/java/ohtu/KirjaDao.java
+++ src/main/java/ohtu/KirjaDao.java
@@ -18,11 +18,10 @@ public class KirjaDao {
 
         Kirja kirja = new Kirja(kirjoittaja, otsikko);
         Connection conn = DriverManager.getConnection(tietokantaosoite);
-        PreparedStatement stmt = conn.prepareStatement("INSERT INTO Kirja(kirjoittaja, otsikko, tyyppi) "
-                + "VALUES ( ?, ?, ? )");
+        PreparedStatement stmt = conn.prepareStatement("INSERT INTO Kirja(kirjoittaja, otsikko) "
+                + "VALUES ( ?, ? )");
         stmt.setString(1, kirja.getKirjoittaja());
         stmt.setString(2, kirja.getOtsikko());
-        stmt.setString(3, kirja.getTyyppi());
         stmt.execute();
 
         conn.close();
