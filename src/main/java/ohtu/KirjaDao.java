f2bc5aaebddd98aaa05724a10b7ee34ce9d73d40
Juha Lääperi <juha.laaperi@gmail.com>
2017-11-20 12:39:19 +0200

    KirjaDaoon lisätty toiminnallisuus kaikkien kirjojen hakemiseksi

--- src/main/java/ohtu/KirjaDao.java
+++ src/main/java/ohtu/KirjaDao.java
@@ -1,16 +1,18 @@
 package ohtu;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class KirjaDao {
 
     private String tietokantaosoite;
 
     public KirjaDao(String tietokantaosoite) {
-        
+
         this.tietokantaosoite = tietokantaosoite;
     }
 
@@ -23,8 +25,26 @@ public class KirjaDao {
         stmt.setString(1, kirja.getKirjoittaja());
         stmt.setString(2, kirja.getOtsikko());
         stmt.execute();
-
+        stmt.close();
         conn.close();
+    }
 
+    public List<Kirja> haeKirjat() throws Exception {
+        List<Kirja> kirjat = new ArrayList();
+
+        Connection conn = DriverManager.getConnection(tietokantaosoite);
+        ResultSet rs = conn.createStatement().executeQuery("SELECT * FROM Kirja");
+
+        while (rs.next()) {
+            String otsikko = rs.getString("otsikko");
+            String kirjoittaja = rs.getString("kirjoittaja");
+
+            kirjat.add(new Kirja(otsikko, kirjoittaja));
+        }
+
+        rs.close();
+        conn.close();
+        
+        return kirjat;
     }
 }
