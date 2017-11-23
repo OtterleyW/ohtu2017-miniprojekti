865ca2ec0ec32eb65ff1f6c55226ef6068a1382e
Juha Lääperi <juha.laaperi@gmail.com>
2017-11-23 20:14:32 +0200

    KirjaVinkkiControllerTest: lisää testejä

--- src/test/java/ohtu/controller/KirjaVinkkiControllerTest.java
+++ src/test/java/ohtu/controller/KirjaVinkkiControllerTest.java
@@ -1,5 +1,9 @@
 package ohtu.controller;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+import ohtu.Kirja;
 import ohtu.KirjaDao;
 import org.junit.Before;
 import org.junit.Test;
@@ -21,9 +25,7 @@ public class KirjaVinkkiControllerTest {
 
     @Autowired
     private WebApplicationContext webAppContext;
-
     private MockMvc mockMvc;
-
     private KirjaDao kirjaDao;
 
     @Before
@@ -43,5 +45,62 @@ public class KirjaVinkkiControllerTest {
         mockMvc.perform(get("/vinkit"))
                 .andExpect(status().isOk())
                 .andExpect(model().attributeExists("kirjat"));
+    }
+
+    @Test
+    public void lisattyKirjaLoytyyTietokannasta() throws Exception {
+        String otsikko = UUID.randomUUID().toString().substring(0, 10);
+
+        mockMvc.perform(post("/lisaakirja")
+                .param("kirjoittaja", "Kirjoittava Kirjoittaja")
+                .param("otsikko", otsikko));
+
+        MvcResult result = mockMvc.perform(get("/vinkit")).andReturn();
+
+        Collection<Kirja> kirjat = (Collection) result.getModelAndView().getModel().get("kirjat");
+
+        boolean loytyi = false;
+
+        for (Kirja kirja : kirjat) {
+            if (kirja.getKirjoittaja().equals("Kirjoittava Kirjoittaja") && kirja.getOtsikko().equals(otsikko)) {
+                loytyi = true;
+            }
+        }
+
+        assertTrue(loytyi);
+    }
+
+    @Test
+    public void muokkaussivunStatusOk() throws Exception {
+        String otsikko = UUID.randomUUID().toString().substring(0, 10);
+
+        kirjaDao.lisaaKirja("Olen Kirjailija", otsikko);
+        mockMvc.perform(get("/1/muokkaa"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void kirjanMuokkausToimii() throws Exception {
+        String otsikko = UUID.randomUUID().toString().substring(0, 10);
+        kirjaDao.lisaaKirja("Myös Kirjailija", otsikko);
+        String uusiOtsikko = UUID.randomUUID().toString().substring(0, 15);
+
+        List<Kirja> kirjat = kirjaDao.haeKirjat();
+
+        String id = "";
+
+        for (Kirja kirja : kirjat) {
+            if (kirja.getKirjoittaja().equals("Myös Kirjailija") && kirja.getOtsikko().equals(otsikko)) {
+                id = kirja.getId();
+            }
+        }
+
+        mockMvc.perform(post("/" + id + "/muokkaa_kirjaa")
+                .param("id", id)
+                .param("kirjoittaja", "Minua on muokattu! :)")
+                .param("otsikko", uusiOtsikko));
+
+        assertTrue(kirjaDao.haeKirja(id).getKirjoittaja().equals("Minua on muokattu! :)")
+                && kirjaDao.haeKirja(id).getOtsikko().equals(uusiOtsikko));
     }
 }
