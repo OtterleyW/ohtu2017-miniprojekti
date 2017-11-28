407eb2fc39125cd40fefbc9924498cdc00fd2e2e
Pietari Jukarainen <pjukarainen@gmail.com>
2017-11-28 17:08:06 +0200

    videoDaoControlleriin metodeja, html-sivut videon lisäämiselle ja niiden listaamiselle

--- src/main/java/ohtu/controller/VideoVinkkiController.java
+++ src/main/java/ohtu/controller/VideoVinkkiController.java
@@ -1,9 +1,44 @@
-
 package ohtu.controller;
 
+import ohtu.VideoDao;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class VideoVinkkiController {
+
+    private VideoDao videoDao;
+
+    public VideoVinkkiController() {
+        this.videoDao = new VideoDao("jdbc:sqlite:kirjasto.db");
+    }
+
+    @GetMapping("/videovinkit")
+    public String listaaVideoVinkit(Model model) throws Exception {
+        model.addAttribute("videot", videoDao.haeVideot());
+        return "videovinkit";
+    }
+
+    @GetMapping("/videonlisaaminen")
+    public ModelAndView videonlisaaminen() {
+        return new ModelAndView("video");
+    }
     
+    @PostMapping("/lisaavideo")
+    @ResponseBody
+    public String lisaavideo(@RequestParam(value = "otsikko") String otsikko, @RequestParam(value = "url") String url) {
+
+        try {
+            videoDao.lisaaVideo(otsikko, url);
+        } catch (Exception ex) {
+            return "error";
+        }
+        return "Lisätty video " + otsikko + " url: " + url +  "<a href='/'>(takaisin)</a>";
+    }
+
 }
