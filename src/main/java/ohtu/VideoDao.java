4544b93dd9326c0d1626287ccec0c5330134f345
Pietari Jukarainen <pjukarainen@gmail.com>
2017-11-28 16:14:54 +0200

    Video-taulu, videoDao, videoDaoController, video lisätty

---
+++ src/main/java/ohtu/VideoDao.java
@@ -0,0 +1,102 @@
+package ohtu;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class VideoDao {
+
+    private String tietokantaosoite;
+
+    public VideoDao(String tietokantaosoite) {
+        this.tietokantaosoite = tietokantaosoite;
+    }
+
+    public void lisaaVideo(String otsikko, String url) throws Exception {
+        Video video = new Video(otsikko, url, "0");
+        Connection conn = DriverManager.getConnection(tietokantaosoite);
+        PreparedStatement stmt = conn.prepareStatement("INSERT INTO Video(otsikko, url, luettu) "
+                + "VALUES ( ?, ?, ? )");
+        stmt.setString(1, video.getOtsikko());
+        stmt.setString(2, video.getUrl());
+        stmt.setString(3, video.getLuettu());
+        stmt.execute();
+        stmt.close();
+        conn.close();
+
+    }
+
+    public List<Video> haeVideot() throws Exception {
+        List<Video> videot = new ArrayList();
+
+        Connection conn = DriverManager.getConnection(tietokantaosoite);
+        ResultSet rs = conn.createStatement().executeQuery("SELECT * FROM Video");
+
+        while (rs.next()) {
+            String id = rs.getString("id");
+            String otsikko = rs.getString("otsikko");
+            String url = rs.getString("url");
+            String onkoLuettu = rs.getString("luettu");
+
+            Video v = new Video(otsikko, url, onkoLuettu);
+            v.setId(id);
+
+            videot.add(v);
+        }
+
+        rs.close();
+        conn.close();
+
+        return videot;
+    }
+
+    public Video haeVideo(String id) throws Exception {
+        Connection conn = DriverManager.getConnection(tietokantaosoite);
+        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM Video WHERE id = ?");
+        stmt.setString(1, id);
+        ResultSet rs = stmt.executeQuery();
+
+        Video video = new Video(rs.getString("otsikko"), rs.getString("url"), rs.getString("luettu"));
+        video.setId(id);
+
+        rs.close();
+        conn.close();
+
+        return video;
+    }
+    
+     public void poistaVideo(String id) throws Exception {
+        Connection conn = DriverManager.getConnection(tietokantaosoite);
+        PreparedStatement stmt = conn.prepareStatement("DELETE FROM Video WHERE id = ?");
+        stmt.setString(1, id);
+        stmt.execute();
+        stmt.close();
+        conn.close();
+    }
+
+    public void muokkaaVideota(String id, String otsikko, String url) throws Exception {
+
+        Connection conn = DriverManager.getConnection(tietokantaosoite);
+        PreparedStatement stmt = conn.prepareStatement("UPDATE Video SET otsikko = ?, url = ? WHERE id = ? ");
+        stmt.setString(1, otsikko);
+        stmt.setString(2, url);
+        stmt.setString(3, id);
+        stmt.execute();
+        stmt.close();
+        conn.close();
+    }
+
+    public void muutaOnkoLuettu(String onkoLuettu, String id) throws Exception {
+        Connection conn = DriverManager.getConnection(tietokantaosoite);
+        PreparedStatement stmt = conn.prepareStatement("UPDATE Video SET luettu = ? WHERE id = ? ");
+        stmt.setString(1, onkoLuettu);
+        stmt.setString(2, id);
+        stmt.execute();
+        stmt.close();
+        conn.close();
+    }
+
+}
---
+++ src/main/java/ohtu/controller/VideoVinkkiController.java
@@ -0,0 +1,9 @@
+
+package ohtu.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class VideoVinkkiController {
+    
+}
