4979f81e65dbb18f50185512fa85b28c06690940
Jenni <jenniknurmi@gmail.com>
2017-11-29 10:58:17 +0200

    Videovinkkiä pystyy muokata

--- src/main/java/ohtu/controller/VideoVinkkiController.java
+++ src/main/java/ohtu/controller/VideoVinkkiController.java
@@ -1,9 +1,11 @@
 package ohtu.controller;
 
+import ohtu.Video;
 import ohtu.VideoDao;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -42,6 +44,33 @@ public class VideoVinkkiController {
         return "Lisätty video " + otsikko + " url: " + url +  "<a href='/'>(takaisin)</a>";
         }
            return "Videon nimi tai url ei voi olla tyhjä! <a href='/videonlisaaminen'>(takaisin)</a>";
+    }
+    
+    @GetMapping("/{id}/muokkaavideota")
+    public String muokkaaVideota(Model model, @PathVariable String id) throws Exception {
+        try {
+            Video v = videoDao.haeVideo(id);
+            model.addAttribute("video", v);
+        } catch (Exception ex) {
+            return "error";
+        }
+        return "muokkaa_videota";
+    }
+    
+    @PostMapping("/{id}/muokkaa_videota")
+    @ResponseBody
+    public String muokkaaVideotaTietokantaan(@PathVariable String id, @RequestParam(value = "otsikko") String otsikko, @RequestParam(value = "url") String url) {
+        Boolean muokattu = false;
+        try {
+            muokattu = videoDao.muokkaaVideota(id, otsikko, url);
+        } catch (Exception ex) {
+            return "error";
+        }
+        if (muokattu) {
+            return "Muokattu video " + otsikko + " url " + url + "! " + "<a href='/videovinkit'>(vinkkilistaukseen)</a>";
+        }
+
+        return "Videon nimi tai url ei voi olla tyhjä! <a href='/" + id + "/muokkaavideota'>(takaisin)</a>";
     }
 
 }
