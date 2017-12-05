80104293145323d88b8d959195fdc4ea4991ba55
Pietari Jukarainen <pjukarainen@gmail.com>
2017-12-05 17:50:39 +0200

    html-sivut podcastille ja kontrolleri toteutettu

--- src/main/java/ohtu/controller/DefaultController.java
+++ src/main/java/ohtu/controller/DefaultController.java
@@ -9,22 +9,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 import ohtu.Kirja;
+import ohtu.PodcastDao;
 
 @Controller
 public class DefaultController {
     
     private KirjaDao kirjaDao;
     private VideoDao videoDao;
+    private PodcastDao podcastDao;
     
     public DefaultController() {
         this.kirjaDao = new KirjaDao("jdbc:sqlite:kirjasto.db");
         this.videoDao = new VideoDao("jdbc:sqlite:kirjasto.db");
+        this.podcastDao = new PodcastDao("jdbc:sqlite:kirjasto.db");
     }
 
     @GetMapping("/")
     public String naytaPaasivu(Model model) throws Exception {
         model.addAttribute("kirjat", kirjaDao.haeKirjat());
         model.addAttribute("videot", videoDao.haeVideot());
+        model.addAttribute("podcastit", podcastDao.haePodcastit());
         return "index";
     }
 
@@ -36,6 +40,7 @@ public class DefaultController {
         
         model.addAttribute("kirjat", kirjaDao.haeHakusanaaVastaavat(hakusana));
         model.addAttribute("videot", videoDao.haeHakusanaaVastaavat(hakusana));
+        model.addAttribute("podcastit", podcastDao.haeHakusanaaVastaavat(hakusana));
 
         return "index";
     }
--- src/main/java/ohtu/controller/PodcastVinkkiController.java
+++ src/main/java/ohtu/controller/PodcastVinkkiController.java
@@ -1,18 +1,144 @@
-
 package ohtu.controller;
 
+import ohtu.Podcast;
 import ohtu.PodcastDao;
+import ohtu.Video;
 import ohtu.VideoDao;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class PodcastVinkkiController {
-    
-    private PodcastDao podcastDao; 
+
+    private PodcastDao podcastDao;
     private String viesti; //käyttäjälle näytettävä virheviesti tai ilmoitus
 
     public PodcastVinkkiController() {
         this.podcastDao = new PodcastDao("jdbc:sqlite:kirjasto.db");
         this.viesti = "";
+    }
+
+    @GetMapping("/podcastvinkit")
+    public String listaaPodcastVinkit(Model model) throws Exception {
+        tarkistaOnkoViestia(model);
+        model.addAttribute("podcastit", podcastDao.haePodcastitKuunnellunPerusteella("0"));
+        model.addAttribute("kuunnellutPodcastit", podcastDao.haePodcastitKuunnellunPerusteella("1"));
+        return "podcastvinkit";
+    }
+
+    @GetMapping("/podcastinlisaaminen")
+    public String podcastinlisaaminen(Model model) {
+        tarkistaOnkoViestia(model);
+        return "podcast";
+    }
+
+    @PostMapping("/lisaapodcast")
+    @ResponseBody
+    public RedirectView lisaapodcast(@RequestParam(value = "url") String url, @RequestParam(value = "tekija") String tekija, @RequestParam(value = "kuvaus") String kuvaus) {
+        Boolean lisatty = false;
+        try {
+            lisatty = podcastDao.lisaaPodcast(url, tekija, kuvaus);
+        } catch (Exception ex) {
+            return new RedirectView("/error");
+        }
+        if (lisatty) {
+            viesti = "Lisätty podcast " + tekija + " url: " + url;
+            return new RedirectView("/podcastvinkit");
+        }
+        viesti = "Podcastin tekijä tai url ei voi olla tyhjä!";
+        return new RedirectView("/podcastinlisaaminen");
+    }
+
+    @GetMapping("/{id}/muokkaapodcastia")
+    public String muokkaaPodcastia(Model model, @PathVariable String id) throws Exception {
+        try {
+            Podcast p = podcastDao.haePodcast(id);
+            tarkistaOnkoViestia(model);
+            model.addAttribute("podcast", p);
+        } catch (Exception ex) {
+            return "error";
+        }
+        return "muokkaa_podcastia";
+    }
+
+    @PostMapping("/{id}/muokkaa_podcastia")
+    @ResponseBody
+    public RedirectView muokkaaPodcastiaTietokantaan(@PathVariable String id, @RequestParam(value = "url") String url, @RequestParam(value = "tekija") String tekija) {
+        Boolean muokattu = false;
+        try {
+            muokattu = podcastDao.muokkaaPodcastia(id, url, tekija);
+        } catch (Exception ex) {
+            return new RedirectView("/error");
+        }
+        if (muokattu) {
+            viesti = "Muokattu podcast " + tekija + " url " + url + "!";
+            return new RedirectView("/podcastvinkit");
+        }
+
+        viesti = "Podcastin tekijä tai url ei voi olla tyhjä!";
+        return new RedirectView("/" + id + "/muokkaapodcastia");
+    }
+
+    @GetMapping("/{id}/poistapodcast")
+    public String poistaPodcastVarmistus(Model model, @PathVariable String id) throws Exception {
+        try {
+            Podcast p = podcastDao.haePodcast(id);
+            model.addAttribute("podcast", p);
+        } catch (Exception ex) {
+            return "error";
+        }
+        return "poista_podcast";
+    }
+
+    @PostMapping("/{id}/poista_podcast")
+    @ResponseBody
+    public RedirectView poistaPodcast(@PathVariable String id) throws Exception {
+
+        Podcast p = podcastDao.haePodcast(id);
+        try {
+            podcastDao.poistaPodcast(id);
+        } catch (Exception ex) {
+            return new RedirectView("/error");
+        }
+        viesti = "Poistettu podcast " + p.getTekija() + ", jonka url on " + p.getUrl() + ".";
+        return new RedirectView("/podcastvinkit");
+
+    }
+
+    @GetMapping("/podcast/{id}/info")
+    public String naytaInfo(Model model, @PathVariable String id) throws Exception {
+        try {
+            Podcast p = podcastDao.haePodcast(id);
+            model.addAttribute("podcast", p);
+        } catch (Exception ex) {
+            return "error";
+        }
+        return "podcastin_infosivu";
+    }
+
+    @GetMapping("/{id}/onko_kuunneltu")
+    public RedirectView merkitseOnkoKuunneltu(Model model, @PathVariable String id) throws Exception {
+        try {
+            Podcast p = podcastDao.haePodcast(id);
+            p.merkitseLuetuksi();
+            podcastDao.muutaOnkoLuettu(p.getLuettu(), id);
+
+        } catch (Exception ex) {
+            return new RedirectView("error");
+        }
+        return new RedirectView("/podcastvinkit");
+    }
+
+    private void tarkistaOnkoViestia(Model model) {
+        if (!viesti.isEmpty()) {
+            model.addAttribute("viesti", viesti);
+            viesti = "";
+        }
     }
 }
