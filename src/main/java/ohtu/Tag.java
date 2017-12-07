9f52467e626e3d3fad760e81cfe441df77b4c01b
Jenni <jenniknurmi@gmail.com>
2017-12-07 20:33:13 +0200

    Tagien lisäystä aloitettu

---
+++ src/main/java/ohtu/Tag.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ohtu;
+
+/**
+ *
+ * @author Jenni
+ */
+public class Tag {
+
+    private String nimi;
+    private String id;
+
+    public Tag(String nimi) {
+        this.nimi = nimi;
+    }
+
+    public String getId() {
+        return id;
+    }
+    
+    public void setId(String id){
+        this.id = id;
+    }
+    
+    public String getNimi() {
+        return nimi;
+    }
+
+    public void setNimi(String Nimi) {
+        this.nimi = nimi;
+    }
+}
